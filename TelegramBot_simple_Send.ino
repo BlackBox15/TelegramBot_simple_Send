6566dcd283dc6eb944d0c392969cd1b56844ec40
Kulkov <mk1221@yandex.ru>
2022-02-02 16:42:42 +0500

    Initial commit

---
+++ TelegramBot_simple_Send.ino
@@ -0,0 +1,188 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266TelegramBOT.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+//_______________________ Initialize Telegram BOT
+#define BOTtoken "5214355646:AAEbkjgO9eG7XLAdUax0jDR_VUIlGgFEeso"  //token of TestBOT
+#define BOTname "LilSmartHome"
+#define BOTusername "lilsmarthomebot"
+
+//_______________________ Initialize Wifi connection to the router
+char ssid[] = "Keenetic-5525";                      // your network SSID (name)
+char password[] = "EYZco21EYZco21";                 // your network key
+
+TelegramBOT bot(BOTtoken, BOTname, BOTusername);
+
+int Bot_mtbs = 5000;                                //mean time between scan messages
+long Bot_lasttime;                                  //last time messages' scan has been done
+long offset;
+long chat_id;
+
+Adafruit_BME280 bme;
+
+float temperature, humidity, pressure;
+boolean waterAlertFlag = 0;
+const int analogInPin = A0;
+
+/*********************************************
+*                     setup()
+**********************************************/
+void setup() {
+
+    Serial.begin(115200);
+    bme.begin(0x76);
+    delay(3000);
+    
+    // attempt to connect to Wifi network:
+    Serial.print("Connecting Wifi: ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    IPAddress ip = WiFi.localIP();
+    Serial.println(ip);
+    
+    bot.begin();      // launch Bot functionalities
+    offset = (bot.message[1][0]).toInt();                   // initial setup of an offset-key
+}
+
+/*********************************************
+*                    loop()
+**********************************************/
+void loop() {
+    // amount of messages read per time  (update_id, name_id, name, lastname, chat_id, text)
+    // message   [i]                     [     0,      1,      2,      3,        4,     5   ]
+    
+    if (millis() > Bot_lasttime + Bot_mtbs)  {
+        bot.getUpdates(String(offset));                     // checking new messages by interval
+        executeMessage();
+        printToSerial();                                    // printing our message array to Serial
+        Bot_lasttime = millis();                            // Update next interval
+    }
+    
+//    checkForWater();
+
+}
+
+
+/*********************************************
+*                    checkForWater()
+**********************************************/
+String checkForWater()   {
+    if(analogRead(analogInPin) > 300)    {
+        waterAlertFlag = 1;
+    }
+    else    {
+        waterAlertFlag = 0;
+    }
+}
+
+/*********************************************
+*                    getClimate()
+**********************************************/
+String getClimate()   {
+    String result;
+    
+    temperature = bme.readTemperature();
+    humidity = bme.readHumidity();
+    pressure = bme.readPressure() / 100.0F;
+    
+    result = "T = " + String(temperature) + "°C" + "\t\t";
+    result += "P = " + String(pressure) + "hPa" + "\t\t";
+    result += "H = " + String(humidity) + "%";
+    
+    return result;
+}
+
+
+/*********************************************
+*                    executeMessage()
+**********************************************/
+void executeMessage()   {
+
+    String startMessage = "Wellcome from @lilsmarthomebot, your personal Bot on ESP8266 board.";
+                    //  startMessage += "\/start - Print this message.\r";
+                    //  startMessage += "\/climate - Get the climate info.\r";
+    String climateMessage = getClimate();
+    String waterAlertMessage = "ALERT! WATER!";
+    
+    offset = (bot.message[1][0]).toInt();                   // initial setup of an offset-key
+
+    if (bot.message[1][5] == "\/start") {
+        bot.sendMessage(bot.message[1][4], startMessage, "");
+    }
+    if (bot.message[1][5] == "\/climate") {
+        bot.sendMessage(bot.message[1][4], climateMessage, "");
+    }
+    else    {
+        bot.sendMessage(bot.message[1][4], "Send a correct command. Check it with \/start.", "");
+    }
+
+    if(waterAlertFlag)  {
+        bot.sendMessage(bot.message[1][4], waterAlertMessage, "");
+    }
+
+    
+
+
+//  for (int i = 1; i < bot.message[0][0].toInt() + 1; i++)      {
+//    //        bot.message[i][5]=bot.message[i][5].substring(1,bot.message[i][5].length());
+//
+//    if (bot.message[i][5] == "\/start") {
+//        bot.sendMessage(bot.message[i][4], startMessage, "");
+//    }
+//    if (bot.message[i][5] == "\/climate") {
+//        bot.sendMessage(bot.message[i][4], climateMessage, "");
+//    }
+//    else    {
+//        bot.sendMessage(bot.message[i][4], "Send a correct command. Check it with \/start.", "");
+//    }
+//
+//  }
+
+
+
+    bot.message[0][0] = "";     // All messages have been replied - reset new messages
+    // IDK mb it isn't need
+    offset++;                   // increasing an offset
+}
+
+
+/*********************************************
+*                    printToSerial()
+**********************************************/
+void printToSerial()    {
+    
+    // checking some special method
+    Serial.print("offset =  ");
+    Serial.println(offset);
+    
+    for (int i = 0; i < 3; i++)  {
+        // Printing a message array
+        for (int j = 0; j < 6; j++)  {
+            Serial.print("message[");
+            Serial.print(i);
+            Serial.print("][");
+            Serial.print(j);
+            Serial.print("] = ");
+            Serial.println(bot.message[i][j]);
+        }
+        Serial.println();
+    }
+    
+//    Serial.print("ADC value = ");
+//    Serial.println(analogRead(analogInPin));
+    Serial.println(getClimate());
+    //    bot.sendMessage(bot.message[1][4], "hello! from ESP", "");
+    //    bot.message[0][0] = "";                             // marking all messages as read
+}
